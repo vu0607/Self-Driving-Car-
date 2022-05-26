2b77b02e0d325edad8d39454e9b08877274c9fde
vudangitwork <91715618+vu0607@users.noreply.github.com>
2022-05-26 23:11:13 +0700

    Add files via upload

---
+++ run_model_two.py
@@ -0,0 +1,34 @@
+import cv2
+import scipy.misc
+import random
+from scipy import pi
+
+img = cv2.imread('steering_wheel_image.jpg',0)
+rows,cols = img.shape
+
+smoothed_angle = 0
+
+i = 0
+from subprocess import call
+
+
+from keras.model import load_model
+
+model=load_model("Best_model_Two/mymodel_best_model.ckpt")
+
+while(cv2.waitKey(10) != ord('q')):
+    full_image = scipy.misc.imread("driving_dataset/driving_dataset/" + str(i) + ".jpg", mode="RGB")
+    image = scipy.misc.imresize(full_image[-150:], [66, 200]) / 255.0
+    degrees = model.predict(image[None,...])[0][0] * 180.0 / scipy.pi
+    #call("clear")
+    print("Predicted steering angle: " + str(degrees) + " degrees")
+    cv2.imshow("frame", cv2.cvtColor(full_image, cv2.COLOR_RGB2BGR))
+    #make smooth angle transitions by turning the steering wheel based on the difference of the current angle
+    #and the predicted angle
+    smoothed_angle += 0.2 * pow(abs((degrees - smoothed_angle)), 2.0 / 3.0) * (degrees - smoothed_angle) / abs(degrees - smoothed_angle)
+    M = cv2.getRotationMatrix2D((cols/2,rows/2),-smoothed_angle,1)
+    dst = cv2.warpAffine(img,M,(cols,rows))
+    cv2.imshow("steering wheel", dst)
+    i += 1
+
+cv2.destroyAllWindows()
